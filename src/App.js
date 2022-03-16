99983ae62009e6f2c8fff831678554a67d58fc14
sarahpicard <sarahannrey225@gmail.com>
2022-03-16 15:07:49 -0400

    stub greeting page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeting from './pages/Greeting';
 
 function App() {
   return (
     <div>
-
+      <Greeting />
     </div>
   );
 }
---
+++ src/pages/Greeting.jsx
@@ -0,0 +1,10 @@
+const Greeting = () => {
+  return(
+    <div className="greeting-head">
+      <h1>Sarah A. Picard</h1>
+      <p>Fullstack Software Developer</p>
+    </div>
+  )
+}
+
+export default Greeting
