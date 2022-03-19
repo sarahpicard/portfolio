378e53b3744d6673afc05d0672a482f86aee51bf
sarahpicard <sarahannrey225@gmail.com>
2022-03-19 17:07:58 -0400

    add link hover on nav bar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,13 +1,17 @@
+import './Navbar.css'
+
 const Nav = () => {
   
   return(
-    <div className="flex justify-end space-x-10 pr-12 py-8 nav-text text-3xl nav-bar-total">
+    <div className="flex justify-end pr-12 py-8 text-3xl nav-bar-total">
       <div className="home-nav-button">
         <a href="/">SP</a>
       </div>
-      <a href="/projects">Projects</a>
-      <a href="/about">About</a>
-      <a href="/contact">Contact</a>
+      <div className='space-x-10'>
+        <a href="/projects">Projects</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+      </div>
     </div>
   )
 }
