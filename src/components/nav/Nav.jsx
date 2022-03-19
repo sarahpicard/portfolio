5ba499b34385b6ee01aa95e13d1ee448851544ab
sarahpicard <sarahannrey225@gmail.com>
2022-03-19 15:51:49 -0400

    adjust font styling

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,7 +1,7 @@
 const Nav = () => {
   
   return(
-    <div className="flex justify-end space-x-10 mr-4 py-6">
+    <div className="flex justify-end space-x-10 mr-4 py-6 nav-text text-1xl">
       <a href="/projects">Projects</a>
       <a href="/about">About</a>
       <a href="/contact">Contact</a>
