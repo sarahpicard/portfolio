f7c04a49e2f066cc3052faf1554abb5568fd3db6
sarahpicard <sarahannrey225@gmail.com>
2022-03-19 20:48:24 -0400

    adjust margin on about page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -4,7 +4,7 @@ import './About.css'
 
 const About = () => {
   return (
-    <div className="about-page container mt-12 pt-12 mx-auto">
+    <div className="about-page container mt-10 pt-12 mx-auto">
       <div className="h-2/4 w-8/12 mx-auto">
         <img id="headshot" src="headshot2.png" alt="headshot" className="float-left m-5 mr-10" />
           <p className="p-4"><span className="block text-center text-xl about-headline">New Hampshire-based lover of gardening, food, and exploring new places.</span> <br /> I'm a goal-oriented software engineer who loves solving complex challenges and working within team structures to accomplish project progression. <br /><br /> I'm a believer in laughter and positive thinking. With a non-traditional degree path in Neuropsychology, I bring a fresh prospective to my development projects and am constantly utilizing my skills in teamwork, leadership, project management, and flexibility. With a number of years in team-based leadership positions, I have a strong foundation in communication, an effective eye for detail, and a passion for mentorship and teaching. <br /><br /> I have an active interest in user experience, and creation of tangible solutions making an impact on users. <br /><br /> When I'm not busy writing code, you are likely to find me in my vegetable garden during the summer months, or enjoying a hot cup of tea and a good book when it's a bit cooler.</p>
