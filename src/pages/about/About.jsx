4d5817ce8c344601ed88f5747a923052713d622d
sarahpicard <sarahannrey225@gmail.com>
2022-03-19 20:46:55 -0400

    adjust skils badge colors and font

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -9,33 +9,35 @@ const About = () => {
         <img id="headshot" src="headshot2.png" alt="headshot" className="float-left m-5 mr-10" />
           <p className="p-4"><span className="block text-center text-xl about-headline">New Hampshire-based lover of gardening, food, and exploring new places.</span> <br /> I'm a goal-oriented software engineer who loves solving complex challenges and working within team structures to accomplish project progression. <br /><br /> I'm a believer in laughter and positive thinking. With a non-traditional degree path in Neuropsychology, I bring a fresh prospective to my development projects and am constantly utilizing my skills in teamwork, leadership, project management, and flexibility. With a number of years in team-based leadership positions, I have a strong foundation in communication, an effective eye for detail, and a passion for mentorship and teaching. <br /><br /> I have an active interest in user experience, and creation of tangible solutions making an impact on users. <br /><br /> When I'm not busy writing code, you are likely to find me in my vegetable garden during the summer months, or enjoying a hot cup of tea and a good book when it's a bit cooler.</p>
       </div>
-          <div>
-            <h2>Skills in my Toolbox!</h2>
-            <h3>Languages</h3>
-              <Badge name="CSS3" />
-              <Badge name="HTML5" />
-              <Badge name="JavaScript" />
-              <Badge name="Markdown" />
-            <h3>Libraries & Frameworks</h3>
-              <Badge name="React" />
-              <Badge name="React Router" />
-              <Badge name="jQuery" />
-              <Badge name="Bootstrap" />
-            <h3>Version Control</h3>
-              <Badge name="Git" />
-              <Badge name="GitHub" />
-            <h3>Miscellaneous</h3>
-              <Badge name="Heroku" />
-              <Badge name="JSON" />
-              <Badge name="Visual Studio Code" />
-            <h3>Backend Technologies</h3>
-              <Badge name="MongoDB" />
-              <Badge name="Express" />
-              <Badge name="Node.js" />
-              <Badge name="NPM" />
-              <Badge name="Postman" />
-              <Badge name="Microsoft Azure" />
-          </div>
+      <div className="text-center mb-8 mt-4 toolbox-headline">
+        <h2>Skills in my Toolbox...</h2>
+      </div>
+        <div className="columns-4 w-8/12 mx-auto">
+          <h3 className="">Languages</h3>
+            <Badge name="CSS3" backgroundColor="#c18c5d"/>
+            <Badge name="HTML5" backgroundColor="#495867"/>
+            <Badge name="JavaScript" backgroundColor="#ce796b"/>
+            <Badge name="Markdown"  backgroundColor="#e7ad99" className="break-after-column"/>
+          <h3 className="">Libraries & Frameworks</h3>
+            <Badge name="React" backgroundColor="#495867"/>
+            <Badge name="React Router" backgroundColor="#C18c5d"/>
+            <Badge name="jQuery" backgroundColor="#ce796b"/>
+            <Badge name="Bootstrap" backgroundColor="#e7ad99" className="break-after-column"/>
+          <h3 className="">Backend Technologies</h3>
+            <Badge name="MongoDB" backgroundColor="#495867"/>
+            <Badge name="Express" backgroundColor="#e7ad99"/>
+            <Badge name="Node.js" backgroundColor="#c18c5d"/>
+            <Badge name="NPM" backgroundColor="#ce796b"/>
+            <Badge name="Postman" backgroundColor="#495867"/>
+            <Badge name="Microsoft Azure" backgroundColor="#c18c5d" className="break-after-column"/>
+          <h3 className="">Version Control</h3>
+            <Badge name="Git" backgroundColor="#ce796b"/>
+            <Badge name="GitHub" backgroundColor="#495867"/>
+          <h3>Miscellaneous</h3>
+            <Badge name="Heroku" backgroundColor="#e7ad99"/>
+            <Badge name="JSON" backgroundColor="#c18c5d"/>
+            <Badge name="Visual Studio Code" backgroundColor="#ce796b" className="break-after-column"/>
+      </div>
     </div>
   )
 }
