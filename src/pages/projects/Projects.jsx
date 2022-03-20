612c0524854a8e615970cca3e45127c1dbf14090
sarahpicard <sarahannrey225@gmail.com>
2022-03-19 21:23:12 -0400

    add video player for project page

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,9 +1,15 @@
 import './Projects.css'
+import ReactPlayer from 'react-player'
 
 const Projects = () => {
   return (
-    <div>
-      <h4>I'm a project!</h4>
+    <div className='container mx-auto'>
+      <div className='w-8/12 mx-auto'>
+        <h1>IndexCRM</h1>
+        <div>
+          <ReactPlayer url='https://www.youtube.com/watch?v=zEBr8YSF3T8' playing={true} loop={true}/>
+        </div>
+      </div>
     </div>
   )
 }
