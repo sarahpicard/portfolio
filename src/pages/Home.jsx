1c121e37431893d1cfe60fed60d06d42154b21aa
sarahpicard <sarahannrey225@gmail.com>
2022-03-19 21:00:19 -0400

    adjust homepage styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Greeting = () => {
     <div className="block pt-20 greeting-block">
       <h1 className="text-8xl flex justify-center name-text my-5">Sarah A. Picard</h1>
       <p className="flex justify-center role-text text-4xl mb-12">Fullstack Software Developer</p>
-      <div className="flex justify-center space-x-9">
+      <div className="flex justify-center space-x-12 social-links">
         <a href="https://www.linkedin.com/in/sarah-picard/">
           <FontAwesomeIcon icon={faLinkedin} size='2x' />
         </a>
