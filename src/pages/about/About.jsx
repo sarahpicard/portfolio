7aab4b356511c2b28f4a5748cff63b283c356ce7
sarahpicard <sarahannrey225@gmail.com>
2022-03-22 08:39:57 -0400

    adjust styling for project view and add padding to skills on about view

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -4,7 +4,7 @@ import './About.css'
 
 const About = () => {
   return (
-    <div className="about-page container mt-10 pt-12 mx-auto">
+    <div className="about-page container mt-4 pt-12 mx-auto">
       <div className="h-2/4 w-8/12 mx-auto">
         <img id="headshot" src="headshot2.png" alt="headshot" className="float-left m-5 mr-10" />
           <p className="p-4"><span className="block text-center text-xl about-headline">New Hampshire-based lover of gardening, food, and exploring new places.</span> <br /> I'm a goal-oriented software engineer who loves solving complex challenges and working within team structures to accomplish project progression. <br /><br /> I'm a believer in laughter and positive thinking. With a non-traditional degree path in Neuropsychology, I bring a fresh prospective to my development projects and am constantly utilizing my skills in teamwork, leadership, project management, and flexibility. With a number of years in team-based leadership positions, I have a strong foundation in communication, an effective eye for detail, and a passion for mentorship and teaching. <br /><br /> I have an active interest in user experience, and creation of tangible solutions making an impact on users. <br /><br /> When I'm not busy writing code, you are likely to find me in my vegetable garden during the summer months, or enjoying a hot cup of tea and a good book when it's a bit cooler.</p>
@@ -12,17 +12,12 @@ const About = () => {
       <div className="text-center mb-8 mt-4 toolbox-headline">
         <h2>Skills in my Toolbox...</h2>
       </div>
-        <div className="columns-4 w-8/12 mx-auto">
+        <div className="columns-4 w-8/12 mx-auto px-4 pl-12">
           <h3 className="">Languages</h3>
             <Badge name="CSS3" backgroundColor="#c18c5d"/>
             <Badge name="HTML5" backgroundColor="#495867"/>
             <Badge name="JavaScript" backgroundColor="#ce796b"/>
             <Badge name="Markdown"  backgroundColor="#e7ad99" className="break-after-column"/>
-          <h3 className="">Libraries & Frameworks</h3>
-            <Badge name="React" backgroundColor="#495867"/>
-            <Badge name="React Router" backgroundColor="#C18c5d"/>
-            <Badge name="jQuery" backgroundColor="#ce796b"/>
-            <Badge name="Bootstrap" backgroundColor="#e7ad99" className="break-after-column"/>
           <h3 className="">Backend Technologies</h3>
             <Badge name="MongoDB" backgroundColor="#495867"/>
             <Badge name="Express" backgroundColor="#e7ad99"/>
@@ -37,6 +32,11 @@ const About = () => {
             <Badge name="Heroku" backgroundColor="#e7ad99"/>
             <Badge name="JSON" backgroundColor="#c18c5d"/>
             <Badge name="Visual Studio Code" backgroundColor="#ce796b" className="break-after-column"/>
+          <h3 className="">Libraries & Frameworks</h3>
+            <Badge name="React" backgroundColor="#495867"/>
+            <Badge name="React Router" backgroundColor="#C18c5d"/>
+            <Badge name="jQuery" backgroundColor="#ce796b"/>
+            <Badge name="Bootstrap" backgroundColor="#e7ad99" className="break-after-column"/>
       </div>
     </div>
   )
