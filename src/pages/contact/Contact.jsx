6e946d828ef3395bca45376d252846ed27537b5e
sarahpicard <sarahannrey225@gmail.com>
2022-04-18 11:41:58 -0400

    adjust media queries for contact page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -8,13 +8,13 @@ const Contact = () => {
     <div className="flex justify-center">
       <div className="footer block text-center">
         <h4 className="text-4xl mb-12 mt-12">Let's get in touch.</h4>
-        <a className="footer-icons ml-8 text-2xl" href="https://www.linkedin.com/in/sarah-picard/">
-          <FontAwesomeIcon icon={faLinkedin} size='2x' /> sarah-picard
+        <a className="footer-icons ml-8 text-md lg:text-2xl" href="https://www.linkedin.com/in/sarah-picard/">
+          <FontAwesomeIcon icon={faLinkedin} size='2xl' /> sarah-picard
         </a>
-        <a className="footer-icons px-12 text-2xl" href="https://github.com/sarahpicard">
-          <FontAwesomeIcon icon={faGithub} size='2x' /> sarahpicard
-        </a>
-        <FontAwesomeIcon icon={faEnvelope} size='2x' /> <span className="text-2xl">sarahannrey225@gmail.com</span>
+        <a className="footer-icons px-6 text-md lg:text-2xl lg:px-12" href="https://github.com/sarahpicard">
+          <FontAwesomeIcon icon={faGithub} size='2xl' /> sarahpicard
+        </a><br />
+        <FontAwesomeIcon icon={faEnvelope} size='2xl' className="mt-8"/> <span className="text-md lg:text-2xl">sarahannrey225@gmail.com</span>
       </div>
     </div>
   )
